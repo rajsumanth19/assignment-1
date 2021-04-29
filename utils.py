6ca4665f492152889922524aab0083721830afb0
rajsumanth19 <77365156+rajsumanth19@users.noreply.github.com>
2021-04-29 23:58:22 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,32 +1,9 @@
-"""
-#################################
-Util functions such as
-    1) Playing video
-    2) Getting FPS
-    3) Extracting Frames
-    4) Resizing frames
-    5) Renaming files of a directory
-#################################
-"""
-
-#########################################################
-# import libraries
-
 import os
 import re
 import cv2
 from config import new_size
 
-
-#########################################################
-# Function definition
-
 def play_vid(path_vid):
-    """
-    This function plays the imported vide based on the path.
-    :param path_vid: The path of the vide
-    :return: None
-    """
     cap = cv2.VideoCapture(path_vid)
     while cap.isOpened():
         ret, frame = cap.read()
@@ -39,11 +16,6 @@ def play_vid(path_vid):
 
 
 def get_fps(path_vid):
-    """
-    This function return the recorded FPS of the vide.
-    :param path_vid: The path of the vide
-    :return: The video file's FPS
-    """
     cap = cv2.VideoCapture(path_vid)
     fps = cap.get(cv2.CAP_PROP_FPS)
     cap.release()
@@ -52,12 +24,6 @@ def get_fps(path_vid):
 
 
 def vid_to_frame(path_vid, mode):
-    """
-    Extracting the frames from the video file.
-    :param path_vid: The path of the video
-    :param mode: Based on the opened file for this project 1) Fire, 2) No-Fire:Lake Mary, 3) Extracting for test set
-    :return: None
-    """
     vidcap = cv2.VideoCapture(path_vid)
     success, image = vidcap.read()
     count = 0
@@ -67,26 +33,16 @@ def vid_to_frame(path_vid, mode):
         elif mode == 'Lake_Mary':
             cv2.imwrite("frames/lakemary/lake_frame%d.jpg" % count, image)  # Save JPG file
         elif mode == 'Test_Frame':
-            # cv2.imwrite("frames/Test_frame/Fire/test_fire_frame%d.jpg" % count, image)  # Save JPG file for FIRE
             cv2.imwrite("frames/Test_frame/No_Fire/test_nofire_frame%d.jpg" % count, image)
-            # Save JPG file for NO_FIRE, for NO Fire frames uncomment this line and comment the previous line
         success, image = vidcap.read()
         print('Extract new frame: ', success, ' frame = ', count)
         count += 1
 
 
 def resize(path_all, path_resize, mode):
-    """
-    Resizing the imported images to the project and save them on drive based on the dimension parameter.
-    :param path_all: The directory of loaded images to the project
-    :param path_resize: The directory to save the resized files
-    :param mode: Fire, No_Fire(lake mary), or the test data
-    :return: None
-    """
     image_names_dir = os.listdir(path_all)
     if mode == 'Test_Frame':
-        # image_names_dir = os.listdir(path_all + 'Fire')  # This is for the FIRE DIR (Test)
-        image_names_dir = os.listdir(path_all + 'No_Fire')  # This is for the No_FIRE DIR (Test)
+        image_names_dir = os.listdir(path_all + 'No_Fire')  
     image_names_dir.sort()
     new_width = new_size.get('width')
     new_height = new_size.get('height')
@@ -94,8 +50,6 @@ def resize(path_all, path_resize, mode):
 
     count = 0
     for image in image_names_dir:
-        # print(resized_img.shape)
-        # cv2.imshow('output', resized_img)
         if mode == 'Fire':
             img = cv2.imread(path_all + '/' + image)
             resized_img = cv2.resize(img, dimension, interpolation=cv2.INTER_AREA)
@@ -105,23 +59,15 @@ def resize(path_all, path_resize, mode):
             resized_img = cv2.resize(img, dimension, interpolation=cv2.INTER_AREA)
             cv2.imwrite(path_resize + '/lake_resized_' + image, resized_img)
         elif mode == 'Test_Frame':
-            # img = cv2.imread(path_all + 'Fire/' + image)
             img = cv2.imread(path_all + 'No_Fire/' + image)
             resized_img = cv2.resize(img, dimension, interpolation=cv2.INTER_AREA)
-            # cv2.imwrite(path_resize + 'Fire/resized_' + image, resized_img)  # Resize for Fire (Test Data)
-            cv2.imwrite(path_resize + 'No_Fire/resized_' + image, resized_img)  # Resize for NoFire (Test Data)
+            cv2.imwrite(path_resize + 'No_Fire/resized_' + image, resized_img)  
 
-        print('Image Resized ' + str(count) + ' : resized_' + image)
+        print('Image after Resizing ' + str(count) + ' : resized_' + image)
         count += 1
 
 
 def rename_all_files(path=None):
-    """
-    This function returns all the files included in the path directory. This function is used for the fire segmentation
-    challenge to have the same name for both the frame and the peered mask.
-    :param path: The input directory to rename the included files
-    :return: None
-    """
     regex = re.compile(r'\d+')
     if path is "Image":
         path_dir = "frames/Segmentation/Data/Images"
