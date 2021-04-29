48b2e301d236e35d1e54f19c00751b89dbce7081
rajsumanth19 <77365156+rajsumanth19@users.noreply.github.com>
2021-04-29 23:46:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-"""
-Created on Aug. 12, 2020
-@author: Alireza Shamsoshoara
-@Project: Aerial image dataset for fire classification, segmentation, and scheduling using
-          Unmanned Aerial Vehicles (UAVs)
-          Paper: ### TODO: WILL UPDATE HERE AFTER ACCEPTANCE ...
-          Arxiv: https://arxiv.org/pdf/2012.14036.pdf
-          Dataset: https://ieee-dataport.org/open-access/flame-dataset-aerial-imagery-pile-burn-detection-using-drones-uavs
-          YouTube Video: https://www.youtube.com/watch?v=bHK6g37_KyA
-@Northern Arizona University
-This project is developed and tested with Python 3.6 using pycharm on Ubuntu 18.04 LTS machine
-"""
-
-#################################
-# Main File
-#################################
-
-# ############# import libraries
-# General Modules
-
-# Customized Modules
 from config import Mode
 from config import Flags
 
@@ -44,13 +23,9 @@ from training import train_keras
 from classification import classify
 from segmentation import segmentation_keras_load
 
-# from plotdata import plot_scheduling
-# from scheduling import uav_scheduling
-
 
 def main():
     fps = get_fps(path_vid)
-    print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
     if Flags.get('playVideoFlag'):
         play_vid(path_vid)
     if Flags.get('SaveRawFrameFlag'):
@@ -85,8 +60,5 @@ if __name__ == "__main__":
         rename_all_files(path="Mask")
     elif Mode == 'Segmentation':
         segmentation_keras_load()
-    # elif Mode == 'Scheduling':
-    #     uav_scheduling()
-    #     plot_scheduling()
     else:
-        print("Mode is not correct")
+        print("Mode is wrong")
